86912290b29e1aba5c24202d6289bb8b742d14ea
sudheesh4 <sud4decrypt@gmail.com>
2023-12-13 19:56:21 -0500

    Add files via upload

--- finetune.py
+++ finetune.py
@@ -20,6 +20,39 @@ from peft import prepare_model_for_kbit_training,LoraConfig, get_peft_model,Peft
 
 from datasets import load_dataset
 
+def getconfig():
+  bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+  )
+
+  lora_config = LoraConfig(
+    r=8,
+    lora_alpha=32,
+    #target_modules=["query_key_value"],
+    target_modules=["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj", "self_attn.o_proj"], #specific to Llama models.
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+  )
+  return bnb_config, lora_config
+
+def getmodeltoken(model_id):
+    bnb_config, _= getconfig()
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
+    
+    return (model,tokenizer)
+    
+def getnames(model_id,author="TimelyFormulation74"):
+    base_model_name = model_id.split("/")[-1]
+    adapter_model = f"{author}/{base_model_name}-fine-tuned-adapters"
+    new_model = f"{author}/{base_model_name}-fine-tuned"
+    return (adapter_model,new_model)
 
 def print_trainable_parameters(model):
     trainable_params = 0
@@ -32,12 +65,12 @@ def print_trainable_parameters(model):
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
 
-def mystream(user_prompt,model):
+def mystream(user_prompt,model,tokenizer):
     model.config.use_cache = True
     model.eval()
     runtimeFlag = "cuda:0"
     system_prompt = 'You are a helpful assistant that provides accurate and concise responses'
-
+    streamer = TextStreamer(tokenizer)
     B_INST, E_INST = "[INST]", "[/INST]"
     B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 
@@ -45,23 +78,28 @@ def mystream(user_prompt,model):
 
     inputs = tokenizer([prompt], return_tensors="pt").to(runtimeFlag)
     
-    _ = model.generate(**inputs, streamer=streamer, max_new_tokens=500)
+    res = model.generate(**inputs, streamer=streamer, max_new_tokens=500)
+    print(tokenizer.decode(res[0]))
+    return res
+
 
 def checkmodel(model_id,prompt):
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
+    #tokenizer = AutoTokenizer.from_pretrained(model_id)
+    #bnb_config, _= getconfig()
+    #model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
     
+    model,tokenizer=getmodeltoken(model_id)
+
     #generator = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
     #generator("What is Aristotle's approach to logic?")
 
-    mystream(prompt,model)
+    mystream(prompt,model,tokenizer)
 
     del model
 
 def savetohub(model_id,model,author="TimelyFormulation74"):
     base_model_name = model_id.split("/")[-1]
-    adapter_model = f"{author}/{base_model_name}-fine-tuned-adapters" 
-    new_model = f"{author}/{base_model_name}-fine-tuned" 
+    adapter_model , new_model = getnames(model_id,author)
     
     #model.save_pretrained(adapter_model, push_to_hub=True, use_auth_token=True)
     #model.push_to_hub(adapter_model, use_auth_token=True)
@@ -75,27 +113,9 @@ def savetohub(model_id,model,author="TimelyFormulation74"):
 
 
 def myTrain(model_id,data):
-
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.bfloat16
-    )
+    bnb_config, lora_config= getconfig()
     
-    lora_config = LoraConfig(
-        r=8,
-        lora_alpha=32,
-        #target_modules=["query_key_value"],
-        target_modules=["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj", "self_attn.o_proj"], #specific to Llama models.
-        lora_dropout=0.05,
-        bias="none",
-        task_type="CAUSAL_LM"
-    )
-
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
+    model,tokenizer=getmodeltoken(model_id)
 
     data = data.map(lambda samples: tokenizer(samples["text"]), batched=True)
     
@@ -124,14 +144,14 @@ def myTrain(model_id,data):
     
     model.config.use_cache = False 
     trainer.train()
-
+    adapter_model, newmodel_id = getnames(model_id)
     model = PeftModel.from_pretrained(
         model,
         adapter_model,
     )
     model = model.merge_and_unload()
     
-    newmodel_id=savetohub(model_id,model)
+    #newmodel_id=savetohub(model_id,model)
 
     return (model,tokenizer,newmodel_id)
 
